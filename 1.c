89b295ca714779e74cfa7e21846d6efb1df62397
root <nobody@localhost>
2023-03-21 20:39:55 +0800

    [WIP] add icmp xlat, checksum is to be correct

--- 1.c
+++ 1.c
@@ -14,6 +14,7 @@
 #include <netinet/ip6.h>
 #include <netinet/if_ether.h>
 #include <errno.h>
+#include <linux/ipv6.h>
 
 
 
@@ -265,6 +266,7 @@ static __always_inline uint64_t checksum_fold(uint64_t cksum){
 }
 
 static __always_inline long checksum_fill_delta(struct xdp_md *pbf, void *transp_hdr, uint8_t proto, uint64_t checksum_diff){
+	uint64_t delta_checksum = 0;
 	if(proto == IPPROTO_TCP || proto == IPPROTO_UDP){
 		uint16_t *checksum_ptr = NULL;
 		if(proto == IPPROTO_TCP){
@@ -273,14 +275,61 @@ static __always_inline long checksum_fill_delta(struct xdp_md *pbf, void *transp
 		}else if(proto == IPPROTO_UDP){
 			struct udphdr *udph = (struct udphdr *)transp_hdr;
 			checksum_ptr = &udph->check;
+		}else if(proto == IPPROTO_ICMPV6){
+			struct icmp6_hdr *icmp6h = (struct icmp6_hdr *)transp_hdr;
+			checksum_ptr = &icmp6h->icmp6_cksum;
 		}
 		bpf_xdp_valid_ptr(pbf, checksum_ptr);
+		delta_checksum += 0xffff ^ *checksum_ptr;
 		checksum_diff += (0xffff ^ *checksum_ptr);
 		checksum_diff = checksum_fold(checksum_diff);
 		if(checksum_diff == 0xffff && proto == IPPROTO_UDP) checksum_diff = 0;
 		*checksum_ptr = 0xffff ^ checksum_diff;
+		delta_checksum += *checksum_ptr;
 	}
-	return 0;
+	return delta_checksum;
+}
+
+enum {
+	ICMP4_XLAT_DROP = 0,
+	ICMP4_XLAT_OK = 1,
+	ICMP4_XLAT_PAYLOAD = 2
+};
+
+static __always_inline long icmp4_should_trans(struct xdp_md *pbf, const struct icmphdr *icmph){
+	bpf_xdp_valid_ptr(pbf, icmph);
+	if(icmph->type == ICMP_ECHO || icmph->type == ICMP_ECHOREPLY){
+		return ICMP4_XLAT_OK;
+	}else if(icmph->type == ICMP_DEST_UNREACH){
+		if(
+			(icmph->code >= 0 && icmph->code <= 13 && icmph->code != ICMP_UNREACH_SRCFAIL) ||
+			icmph->code == ICMP_UNREACH_PRECEDENCE_CUTOFF
+		){
+			goto xlat_payload;
+		}else{
+			return ICMP4_XLAT_DROP;
+		}
+	}else if(icmph->type == ICMP_TIME_EXCEEDED){
+		goto xlat_payload;
+	}else if(icmph->type == ICMP_PARAMETERPROB){
+		if(icmph->code == 0 || icmph->code == 2){
+			if(
+				(((struct icmp *)icmph)->icmp_pptr >= 0 && ((struct icmp *)icmph)->icmp_pptr <= 3 ) ||
+				(((struct icmp *)icmph)->icmp_pptr >= 8 && ((struct icmp *)icmph)->icmp_pptr <= 9 ) ||
+				(((struct icmp *)icmph)->icmp_pptr >= 12 && ((struct icmp *)icmph)->icmp_pptr <= 19 )
+			){
+				goto xlat_payload;
+			}else{
+				return ICMP4_XLAT_DROP;
+			}
+		}else{
+			return ICMP4_XLAT_DROP;
+		}
+	}else{
+		return ICMP4_XLAT_DROP;
+	}
+	xlat_payload:
+	return ICMP4_XLAT_PAYLOAD;
 }
 
 static __always_inline long fill_ipv6_hdr(struct ip6_hdr *ip6h, const struct iphdr *iph, const struct ipv4_nat_table_value *dst, const struct ipv4_nat_table_value *src, void **transp_hdr, struct xdp_md *pbf){
@@ -311,6 +360,128 @@ static __always_inline long fill_ipv6_hdr(struct ip6_hdr *ip6h, const struct iph
 		ip6f->ip6f_reserved = 0;
 		ip6f->ip6f_offlg = bpf_htons((bpf_ntohs(iph->frag_off) & IP_OFFMASK) << 3 ) | (bpf_ntohs(iph->frag_off) & IP_MF ? IP6F_MORE_FRAG : 0);
 		ip6f->ip6f_ident = bpf_htonl(bpf_ntohs(iph->id));
+	}
+	return checksum;
+}
+
+static __always_inline long fill_icmp6_hdr(struct icmp6_hdr *icmp6h, const struct icmphdr *icmph, struct xdp_md *pbf){
+	uint64_t checksum = 0;
+	if(LIKELY(!!pbf)){
+		bpf_xdp_valid_ptr(pbf, icmp6h);
+	}
+	checksum += 0xffff ^ (*(uint16_t *)icmph);
+	checksum += 0xffffffff ^ (*((uint32_t *)icmph + 1));
+	switch(icmph->type){
+		case ICMP_ECHO:
+			icmp6h->icmp6_type = ICMP6_ECHO_REQUEST;
+			icmp6h->icmp6_code = icmph->code;
+			icmp6h->icmp6_id   = icmph->un.echo.id;
+			icmp6h->icmp6_seq  = icmph->un.echo.sequence;
+			break;
+		case ICMP_ECHOREPLY:
+			icmp6h->icmp6_type = ICMP6_ECHO_REPLY;
+			icmp6h->icmp6_code = icmph->code;
+			icmp6h->icmp6_id   = icmph->un.echo.id;
+			icmp6h->icmp6_seq  = icmph->un.echo.sequence;
+			break;
+		case ICMP_DEST_UNREACH:
+			icmp6h->icmp6_type = ICMP6_DST_UNREACH;
+			icmp6h->icmp6_data32[0] = 0;
+			switch(icmph->code){
+				case ICMP_UNREACH_NET:
+				case ICMP_UNREACH_HOST:
+					icmp6h->icmp6_code = ICMP6_DST_UNREACH_NOROUTE;
+					break;
+				case ICMP_PROT_UNREACH:
+					icmp6h->icmp6_type = ICMP6_PARAM_PROB;
+					icmp6h->icmp6_code = ICMP6_PARAMPROB_NEXTHEADER;
+					icmp6h->icmp6_pptr = bpf_htonl(offsetof(struct ip6_hdr, ip6_nxt));
+				case ICMP_PORT_UNREACH:
+					icmp6h->icmp6_code = ICMP6_DST_UNREACH_NOPORT;
+					break;
+				case ICMP_FRAG_NEEDED:
+					icmp6h->icmp6_type = ICMP6_PACKET_TOO_BIG;
+					icmp6h->icmp6_code = 0;
+					// Here we do not add 20 to mtu, for safety
+					icmp6h->icmp6_mtu  = bpf_htonl(bpf_ntohs(icmph->un.frag.mtu) < IPV6_MIN_MTU ? IPV6_MIN_MTU : bpf_ntohs(icmph->un.frag.mtu));
+					break;
+				case ICMP_NET_UNKNOWN:
+				case ICMP_HOST_UNKNOWN:
+				case ICMP_HOST_ISOLATED:
+					icmp6h->icmp6_code = ICMP6_DST_UNREACH_NOROUTE;
+					break;
+				case ICMP_NET_ANO:
+				case ICMP_HOST_ANO:
+				case ICMP_PREC_CUTOFF:
+					icmp6h->icmp6_code = ICMP6_DST_UNREACH_ADMIN;
+					break;
+				default:
+					goto out_drop;
+			}
+			break;
+		case ICMP_TIME_EXCEEDED:
+			icmp6h->icmp6_type = ICMP6_TIME_EXCEEDED;
+			icmp6h->icmp6_code = icmph->code;
+			icmp6h->icmp6_data32[0] = 0;
+			break;
+		case ICMP_PARAMETERPROB:
+			icmp6h->icmp6_type = ICMP6_PARAM_PROB;
+			switch (icmph->code) {
+				case 0:
+				case 2:
+					icmp6h->icmp6_code = ICMP6_PARAMPROB_HEADER;
+					int result_pptr = 0;
+					switch (((struct icmp *)icmph)->icmp_pptr) {
+						case 0:
+							result_pptr = offsetof(struct ip6_hdr, ip6_vfc);
+							break;
+						case 1:
+							result_pptr = 1;
+							break;
+						case 2: case 3:
+							result_pptr = offsetof(struct ip6_hdr, ip6_plen);
+							break;
+						case 8:
+							result_pptr = offsetof(struct ip6_hdr, ip6_hlim);
+							break;
+						case 9:
+							result_pptr = offsetof(struct ip6_hdr, ip6_nxt);
+							break;
+						case 12: case 13: case 14: case 15:
+							result_pptr = offsetof(struct ip6_hdr, ip6_src);
+							break;
+						case 16: case 17: case 18: case 19:
+							result_pptr = offsetof(struct ip6_hdr, ip6_dst);
+							break;
+						default:
+							goto out_drop;
+					}
+					icmp6h->icmp6_pptr = bpf_htonl(result_pptr);
+					break;
+				default:
+					goto out_drop;
+			}
+			break;
+	}
+	checksum += *(uint16_t *)icmp6h;
+	checksum += *((uint32_t *)icmp6h + 1);
+	return checksum;
+out_drop:
+	return -EINVAL;
+
+}
+
+static __always_inline uint64_t calc_icmpv6_pseudo_checksum(const struct ip6_hdr *ip6h){
+	uint64_t checksum = 0;
+	#pragma unroll
+	for(int i = 0; i < 4; i++){
+		checksum += ip6h->ip6_src.s6_addr32[i];
+		checksum += ip6h->ip6_dst.s6_addr32[i];
+	}
+	if(ip6h->ip6_nxt != IPPROTO_FRAGMENT){
+		checksum += ip6h->ip6_plen;
+	}else{
+		checksum += bpf_htons(bpf_ntohs(ip6h->ip6_plen) - sizeof(struct ip6_frag));
 	}
 	return checksum;
 }
@@ -347,11 +518,61 @@ static __always_inline long ipv4_to_6(struct xdp_md *pbf, size_t offset){
 	}
 
 	ssize_t len_diff = calc_extra_space_ip4_hdr(iph);
-	if(len_diff){
+
+	long icmp_trans_type = 0;
+	struct icmphdr _icmph;
+	struct icmphdr *icmph = &_icmph;
+	struct iphdr _icmp_iph;
+	struct iphdr *icmp_iph = &_icmp_iph;
+	const struct ipv4_nat_table_value *icmp_ip_src, *icmp_ip_dst;
+	ssize_t icmp_len_diff;
+	if(UNLIKELY(iph->protocol == IPPROTO_ICMP)){
+		struct icmphdr *icmph = (struct icmphdr *)(pbf->data + offset + iph->ihl * 4);
+		icmp_trans_type = icmp4_should_trans(pbf, icmph);
+		if(icmp_trans_type == ICMP4_XLAT_DROP){
+			return -EINVAL;
+		}else if(icmp_trans_type == ICMP4_XLAT_PAYLOAD){
+			struct iphdr *icmp_iph = (struct iphdr *)(icmph + 1);
+			if(UNLIKELY((rc = bpf_xdp_ensure_ipv4_header(pbf, (void *)icmp_iph - (void *)(long)pbf->data)) < 0)){
+				return rc;
+			}
+			/* In icmp error payload, we lookup src address in dst map*/
+			icmp_ip_src = ipv4_siit_table_lookup((const struct in_addr *)&icmp_iph->saddr, &dst_4to6_map);
+			if(UNLIKELY(icmp_ip_src == NULL)){
+				return -EINVAL;
+			}
+			if(UNLIKELY(!is_ipv4_siit_table_entry_valid(icmp_ip_src))){
+				return -EINVAL;
+			}
+			icmp_ip_dst = ipv4_siit_table_lookup((const struct in_addr *)&icmp_iph->daddr, &src_4to6_map);
+			if(UNLIKELY(icmp_ip_dst == NULL)){
+				return -EINVAL;
+			}
+			if(UNLIKELY(!is_ipv4_siit_table_entry_valid(icmp_ip_dst))){
+				return -EINVAL;
+			}
+			_icmp_iph = *icmp_iph;
+			icmp_len_diff = calc_extra_space_ip4_hdr(icmp_iph);
+			len_diff += icmp_len_diff;
+		}else{ /*Do nothing*/
+
+		}
+		bpf_xdp_valid_ptr(pbf, icmph);
+		_icmph = *icmph;
+		iph->protocol = IPPROTO_ICMPV6;
+	}else{ // Fill uninitialized variables to make verifier happy
+		icmp_len_diff = 0;
+		memset(&_icmph, 0, sizeof(_icmph));
+		memset(&_icmp_iph, 0, sizeof(_icmp_iph));
+	}
+	if(LIKELY(len_diff != 0)){
 		rc = bpf_xdp_adjust_head_at(pbf, len_diff, offset);
 		if(UNLIKELY(rc < 0)){
 			return rc;
 		}
+	}
+	if(UNLIKELY(icmp_trans_type == ICMP4_XLAT_PAYLOAD)){
+		iph->tot_len = bpf_htons(bpf_ntohs(iph->tot_len) + icmp_len_diff);
 	}
 	struct ip6_hdr *ip6h = (struct ip6_hdr *)(pbf->data + offset);
 	void *transp_hdr;
@@ -361,9 +582,65 @@ static __always_inline long ipv4_to_6(struct xdp_md *pbf, size_t offset){
 	if(UNLIKELY(rc < 0)){
 		return rc;
 	}
+	uint64_t checksum_diff = 0;
+	checksum_diff += rc;
 
-	uint64_t checksum_diff = rc;
-	rc = checksum_fill_delta(pbf, transp_hdr, iph->protocol, checksum_diff);	
+	if(iph->protocol == IPPROTO_ICMPV6){
+		checksum_diff = calc_icmpv6_pseudo_checksum(ip6h);
+		struct icmp6_hdr *icmp6h = (struct icmp6_hdr *)transp_hdr;
+		rc = fill_icmp6_hdr(icmp6h, icmph, pbf);
+		if(UNLIKELY(rc < 0)){
+			return rc;
+		}
+		checksum_diff += rc;
+		if(icmp_trans_type == ICMP4_XLAT_PAYLOAD){
+			struct ip6_hdr *icmp6_ip6h = (struct ip6_hdr *)(icmp6h + 1);
+			void *inner_transp_hdr;
+			if(icmp_iph -> protocol == IPPROTO_ICMP){
+				icmp_iph->protocol = IPPROTO_ICMPV6;
+				checksum_diff += 0xffff ^ ((IPPROTO_ICMP << 8) | (icmp_iph->ttl));
+				checksum_diff += (icmp_iph->ttl << 8) | (IPPROTO_ICMPV6);
+			}
+			rc = fill_ipv6_hdr(icmp6_ip6h, icmp_iph, icmp_ip_dst, icmp_ip_src, &inner_transp_hdr, pbf);
+			if(UNLIKELY(rc < 0)){
+				return rc;
+			}
+			uint64_t inner_checksum_diff = rc;
+			checksum_diff += rc; // Include Address Diff
+			checksum_diff += 0xffffffff ^ (*((uint32_t *) icmp_iph + 0)); // Cancel IPv4 Ver IHL TOS LEN
+			checksum_diff += 0xffffffff ^ (*((uint32_t *) icmp_iph + 1)); // Cancel IPv4 ID, Flags, Frag Offset
+			checksum_diff += 0xffff ^ icmp_iph->check; // Cancel IPv4 checksum
+			checksum_diff += *((uint32_t *) icmp6_ip6h + 0); //Include IPv6 Ver TC FL
+			checksum_diff += icmp6_ip6h->ip6_plen; //Include IPv6 Payload Length
+			if(icmp6_ip6h->ip6_nxt == IPPROTO_FRAGMENT){ // Include Possible Fragment Header
+				struct ip6_frag *icmp6_ip6f = (struct ip6_frag *)(icmp6_ip6h + 1);
+				checksum_diff += *((uint32_t *) icmp6_ip6f + 0);
+				checksum_diff += *((uint32_t *) icmp6_ip6f + 1);
+			}
+
+			if(icmp_iph->protocol == IPPROTO_ICMP){
+				inner_checksum_diff = 0;
+				struct icmp6_hdr *inner_icmp6h = (struct icmp6_hdr *)inner_transp_hdr;
+				if(inner_icmp6h->icmp6_type == ICMP_ECHO){
+					inner_checksum_diff += (0xffff ^ ICMP_ECHO) + ICMP6_ECHO_REQUEST;
+					inner_icmp6h->icmp6_type = ICMP6_ECHO_REQUEST;
+				}else if(inner_icmp6h->icmp6_type == ICMP_ECHOREPLY){
+					inner_checksum_diff += (0xffff ^ ICMP_ECHOREPLY) + ICMP6_ECHO_REPLY;
+					inner_icmp6h->icmp6_type = ICMP6_ECHO_REPLY;
+				}
+				checksum_diff += inner_checksum_diff; //Include ICMPv6 Type Code Checksum
+				inner_checksum_diff += calc_icmpv6_pseudo_checksum(icmp6_ip6h);
+			}
+			rc = checksum_fill_delta(pbf, inner_transp_hdr, icmp_iph->protocol, inner_checksum_diff);
+			if(UNLIKELY(rc < 0)){
+				return rc;
+			}
+			checksum_diff += rc;
+			
+		}
+	}
+
+	rc = checksum_fill_delta(pbf, transp_hdr, iph->protocol, checksum_diff);
 	if(UNLIKELY(rc < 0)){
 		return rc;
 	}
